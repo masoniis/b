048c16e79e276ad56d4abc80b02af9b53104bf5c
mason <58895787+masoniis@users.noreply.github.com>
2025-09-11 11:40:11 -0600

    refractoring app and systems to better modularize logic

--- src/ecs/systems.rs
+++ src/ecs/systems.rs
@@ -1,42 +1,68 @@
-use crate::ecs::resources::Input;
-use crate::graphics::camera::{Camera, CameraMovement};
+use crate::ecs::world::World;
+use crate::graphics::camera::CameraMovement;
 use winit::event::WindowEvent;
 use winit::keyboard::{KeyCode, PhysicalKey};
 
-pub fn input_system(input: &mut Input, event: &WindowEvent) {
-    if let WindowEvent::KeyboardInput {
-        event: key_event, ..
-    } = event
-    {
-        if let PhysicalKey::Code(key_code) = key_event.physical_key {
-            match key_event.state {
-                winit::event::ElementState::Pressed => {
-                    input.pressed_keys.insert(key_code);
-                }
-                winit::event::ElementState::Released => {
-                    input.pressed_keys.remove(&key_code);
+pub trait System {
+    /// A hook that enables the system to perform actions BEFORE any events
+    /// are processed. Useful for once-per frame actions like clock updates.
+    fn new_events_hook(&mut self, _world: &mut World) {}
+    /// A hook that enables a system to take action in response to events.
+    fn window_event_hook(&mut self, _world: &mut World, _event: &WindowEvent) {}
+}
+
+pub struct InputSystem;
+impl System for InputSystem {
+    fn window_event_hook(&mut self, world: &mut World, event: &WindowEvent) {
+        if let WindowEvent::KeyboardInput {
+            event: key_event, ..
+        } = event
+        {
+            if let PhysicalKey::Code(key_code) = key_event.physical_key {
+                match key_event.state {
+                    winit::event::ElementState::Pressed => {
+                        world.input_resource.pressed_keys.insert(key_code);
+                    }
+                    winit::event::ElementState::Released => {
+                        world.input_resource.pressed_keys.remove(&key_code);
+                    }
                 }
             }
         }
     }
 }
 
-pub fn camera_update_system(camera: &mut Camera, aspect_ratio: f32) {
-    camera.update_view_matrix();
-    camera.update_projection_matrix(aspect_ratio);
+pub struct CameraUpdateSystem;
+impl System for CameraUpdateSystem {
+    fn new_events_hook(&mut self, world: &mut World) {
+        let aspect_ratio = world.window_size.0 as f32 / world.window_size.1 as f32;
+        world.camera.update_view_matrix();
+        world.camera.update_projection_matrix(aspect_ratio);
+    }
 }
 
-pub fn camera_movement_system(camera: &mut Camera, input: &Input, delta_time: f32) {
-    if input.pressed_keys.contains(&KeyCode::KeyW) {
-        camera.process_keyboard(CameraMovement::Forward, delta_time);
-    }
-    if input.pressed_keys.contains(&KeyCode::KeyS) {
-        camera.process_keyboard(CameraMovement::Backward, delta_time);
-    }
-    if input.pressed_keys.contains(&KeyCode::KeyA) {
-        camera.process_keyboard(CameraMovement::Left, delta_time);
-    }
-    if input.pressed_keys.contains(&KeyCode::KeyD) {
-        camera.process_keyboard(CameraMovement::Right, delta_time);
+pub struct CameraMovementSystem;
+impl System for CameraMovementSystem {
+    fn new_events_hook(&mut self, world: &mut World) {
+        if world.input_resource.pressed_keys.contains(&KeyCode::KeyW) {
+            world
+                .camera
+                .process_keyboard(CameraMovement::Forward, world.delta_time.0);
+        }
+        if world.input_resource.pressed_keys.contains(&KeyCode::KeyS) {
+            world
+                .camera
+                .process_keyboard(CameraMovement::Backward, world.delta_time.0);
+        }
+        if world.input_resource.pressed_keys.contains(&KeyCode::KeyA) {
+            world
+                .camera
+                .process_keyboard(CameraMovement::Left, world.delta_time.0);
+        }
+        if world.input_resource.pressed_keys.contains(&KeyCode::KeyD) {
+            world
+                .camera
+                .process_keyboard(CameraMovement::Right, world.delta_time.0);
+        }
     }
 }
--- src/ecs/world.rs
+++ src/ecs/world.rs
@@ -1,1 +1,20 @@
-pub fn dummy() {}
+use crate::ecs::resources::{DeltaTime, Input as InputResource};
+use crate::graphics::camera::Camera;
+
+pub struct World {
+    pub input_resource: InputResource,
+    pub delta_time: DeltaTime,
+    pub camera: Camera,
+    pub window_size: (u32, u32),
+}
+
+impl Default for World {
+    fn default() -> Self {
+        Self {
+            input_resource: InputResource::new(),
+            delta_time: DeltaTime::default(),
+            camera: Camera::default(),
+            window_size: (800, 600),
+        }
+    }
+}
