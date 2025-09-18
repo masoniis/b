87edf6f559e75bac7bc060517cb15aeea609ff35
mason <58895787+masoniis@users.noreply.github.com>
2025-09-17 20:59:30 -0600

    extended input resource API to support distinct key logic (press, held, release)

--- src/ecs/resources/input.rs
+++ src/ecs/resources/input.rs
@@ -3,19 +3,37 @@ use glam::{DVec2, Vec2};
 use std::collections::HashSet;
 use winit::keyboard::KeyCode;
 
-#[derive(Default, Debug, Resource)]
+#[derive(Debug, Resource)]
 pub struct InputResource {
-    pub pressed_keys: HashSet<KeyCode>,
+    pub previous_keys: HashSet<KeyCode>,
+    pub current_keys: HashSet<KeyCode>,
+
     pub mouse_delta: DVec2,
     pub scroll_delta: Vec2,
 }
 
 impl InputResource {
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            previous_keys: HashSet::new(),
+            current_keys: HashSet::new(),
+            mouse_delta: DVec2::ZERO,
+            scroll_delta: Vec2::ZERO,
+        }
     }
 
-    pub fn is_key_pressed(&self, key_code: &KeyCode) -> bool {
-        self.pressed_keys.contains(key_code)
+    /// Was this key just pressed in this frame?
+    pub fn was_key_pressed(&self, key_code: KeyCode) -> bool {
+        self.current_keys.contains(&key_code) && !self.previous_keys.contains(&key_code)
+    }
+
+    /// Is this key currently held down? (distinct from just being pressed)
+    pub fn is_key_down(&self, key_code: KeyCode) -> bool {
+        self.current_keys.contains(&key_code)
+    }
+
+    /// Was this key just released in this frame?
+    pub fn was_key_released(&self, key_code: KeyCode) -> bool {
+        !self.current_keys.contains(&key_code) && self.previous_keys.contains(&key_code)
     }
 }
