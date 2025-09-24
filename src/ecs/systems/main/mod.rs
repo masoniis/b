95b3558d188708f5d1efeff17b11c1a41ee028ff
mason <58895787+masoniis@users.noreply.github.com>
2025-09-24 10:07:52 -0600

    remove old font loading

--- src/ecs/systems/main/mod.rs
+++ src/ecs/systems/main/mod.rs
@@ -15,8 +15,5 @@ pub use camera_control::camera_control_system;
 pub mod screen_diagnostics;
 pub use screen_diagnostics::screen_diagnostics_system;
 
-pub mod text_meshing;
-pub use text_meshing::update_text_mesh_system;
-
 pub mod time;
 pub use time::time_system;
--- src/ecs/systems/main/render/screen_text_renderer.rs
+++ src/ecs/systems/main/render/screen_text_renderer.rs
@@ -3,8 +3,8 @@ use crate::graphics::webgpu_renderer::WebGpuRenderer;
 use bevy_ecs::prelude::{Query, ResMut};
 
 pub fn screen_text_render_system(
-    mut renderer: ResMut<WebGpuRenderer>,
-    query: Query<(&ScreenTextComponent)>,
+    mut _renderer: ResMut<WebGpuRenderer>,
+    _query: Query<&ScreenTextComponent>,
 ) {
     return;
 }
