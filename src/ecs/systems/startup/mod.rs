7d197e0f603e819bd6bea31045447f74971e85a4
mason <58895787+masoniis@users.noreply.github.com>
2025-09-24 10:07:52 -0600

    remove old font loading

--- src/ecs/systems/startup/mod.rs
+++ src/ecs/systems/startup/mod.rs
@@ -1,8 +1,5 @@
 pub mod chunk_generation;
 pub use chunk_generation::chunk_generation_system;
 
-pub mod font_loader;
-pub use font_loader::font_loader_system;
-
 pub mod init_screen_diagnostics;
 pub use init_screen_diagnostics::init_screen_diagnostics_system;
