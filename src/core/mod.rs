acf8963ae6ee0ac89390751215ba41afbcd61160
mason <58895787+masoniis@users.noreply.github.com>
2025-09-26 23:19:14 -0600

    modularized main app in core
    remove the app runner and add it as a static method to app

--- src/core/mod.rs
+++ src/core/mod.rs
@@ -1,3 +1,2 @@
 pub mod app;
 pub mod graphics;
-pub mod runner;
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
-use tracing::{error, info};
-
 pub mod core;
 pub mod ecs_modules;
 pub mod ecs_resources;
 pub mod ecs_systems;
 pub mod utils;
 
+pub use core::app::App;
+
+use tracing::{error, info};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     utils::logger::attach_logger();
     info!("Logger attached...");
     info!("Running app...");
 
-    if let Err(e) = core::runner::run_app() {
+    if let Err(e) = App::run_app() {
         error!("App error: {}", e);
     } else {
         info!("App runner finished without errors!");
