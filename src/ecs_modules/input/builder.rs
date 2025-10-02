e9f40d1811e42127e42d78fc9d4131848d446407
mason <58895787+masoniis@users.noreply.github.com>
2025-10-01 18:29:26 -0600

    added a system set for managing increasing number of systems

--- src/ecs_modules/input/builder.rs
+++ src/ecs_modules/input/builder.rs
@@ -1,20 +1,29 @@
-use super::{systems::main, ActionStateResource, InputActionMapResource, InputResource};
+use super::{
+    resources::{Buttons, CursorMovement},
+    systems::main,
+    ActionStateResource, InputActionMapResource,
+};
 use crate::{
     ecs_bridge::{Plugin, Schedules},
     ecs_modules::input::events::{
         KeyboardInputEvent, MouseButtonInputEvent, MouseMoveEvent, MouseScrollEvent,
         RawDeviceEvent, RawWindowEvent,
     },
+    ecs_modules::CoreSet,
 };
 use bevy_ecs::{event::Events, schedule::IntoScheduleConfigs, world::World};
+use winit::{event::MouseButton, keyboard::PhysicalKey};
 
 pub struct InputModuleBuilder;
 
 impl Plugin for InputModuleBuilder {
     fn build(&self, schedules: &mut Schedules, world: &mut World) {
-        world.insert_resource(InputResource::default());
         world.insert_resource(InputActionMapResource::default());
         world.insert_resource(ActionStateResource::default());
+
+        world.insert_resource(Buttons::<PhysicalKey>::default());
+        world.insert_resource(Buttons::<MouseButton>::default());
+        world.insert_resource(CursorMovement::default());
 
         // External events (comes from the app wrapper)
         world.init_resource::<Events<RawWindowEvent>>();
@@ -26,12 +35,15 @@ impl Plugin for InputModuleBuilder {
         world.init_resource::<Events<MouseScrollEvent>>();
         world.init_resource::<Events<MouseButtonInputEvent>>();
 
-        schedules.input.add_systems((
-            main::window_events_system,
-            main::device_events_system,
-            main::update_action_state_system.after(main::window_events_system),
-            main::update_action_state_system.after(main::device_events_system),
-            main::update_action_state_system,
-        ));
+        schedules.main.add_systems(
+            (
+                main::window_events_system,
+                main::device_events_system,
+                main::update_action_state_system.after(main::window_events_system),
+                main::update_action_state_system.after(main::device_events_system),
+                main::update_action_state_system,
+            )
+                .in_set(CoreSet::Input),
+        );
     }
 }
