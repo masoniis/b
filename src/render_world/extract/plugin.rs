bc90fc5f87e71220156c14d186d0f66fb00219f7
mason <58895787+masoniis@users.noreply.github.com>
2025-10-04 14:20:39 -0600

    extract camera resource from main world

--- src/render_world/extract/plugin.rs
+++ src/render_world/extract/plugin.rs
@@ -1,4 +1,7 @@
-use super::{extract_camera_system, extract_resource_system, RenderTimeResource};
+use super::{
+    extract_meshes::MeshEntityMap, extract_meshes_system, extract_resource_system,
+    RenderCameraResource, RenderTimeResource,
+};
 use crate::{prelude::*, render_world::RenderSchedule};
 use bevy_ecs::prelude::*;
 
@@ -7,10 +10,13 @@ pub struct ExtractModulePlugin;
 impl Plugin for ExtractModulePlugin {
     fn build(&self, schedules: &mut ScheduleBuilder, world: &mut World) {
         world.insert_resource(RenderTimeResource::default());
+        world.insert_resource(RenderCameraResource::default());
+        world.insert_resource(MeshEntityMap::default());
 
         schedules.entry(RenderSchedule::Extract).add_systems((
-            extract_camera_system,
             extract_resource_system::<RenderTimeResource>,
+            extract_resource_system::<RenderCameraResource>,
+            extract_meshes_system,
         ));
     }
 }
--- src/render_world/extract/resources/mod.rs
+++ src/render_world/extract/resources/mod.rs
@@ -1,3 +1,5 @@
+pub mod render_camera;
 pub mod render_time;
 
+pub use render_camera::RenderCameraResource;
 pub use render_time::RenderTimeResource;
